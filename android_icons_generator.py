ece10ee13b7ee88c343915316b1a2cccd96fb719
=Henning Hall <=henning@taxicaller.com>
2018-05-24 10:25:02 +0200

    Python 3 compatible

--- android_icons_generator.py
+++ android_icons_generator.py
@@ -23,21 +23,21 @@ dimensions = (('xxxhdpi', (192, 192)),
               ('mdpi', (48, 48)))
 
 if len(sys.argv) != 3:
-    print ''
-    print 'Generate different dimension of android icons and put it to proper folder.'
-    print 'Support dimensions: '
+    print ('')
+    print ('Generate different dimension of android icons and put it to proper folder.')
+    print ('Support dimensions: ')
     for (resolution, (width, height)) in dimensions:
-        print '  %7s %dx%d' % (resolution, width, height)
-    print ''
-    print 'Usage: %s <raw icon file> <res dir>/<prefix>'
-    print '  `raw icon file` is the high resolution icon file.'
-    print '  `res dir` is your Android project\'s res dir.'
-    print '  `prefix` can be `drawable` or `mipmap`.'
-    print ''
-    print 'Examples:'
-    print '  android_icons_generator.py ic_launcher.png ~/awesome/app/src/main/res/mipmap'
-    print '  android_icons_generator.py ic_person.png ~/awesome/app/src/main/res/drawable'
-    print ''
+        print ('  %7s %dx%d' % (resolution, width, height))
+    print ('')
+    print ('Usage: %s <raw icon file> <res dir>/<prefix>')
+    print ('  `raw icon file` is the high resolution icon file.')
+    print ('  `res dir` is your Android project\'s res dir.')
+    print ('  `prefix` can be `drawable` or `mipmap`.')
+    print ('')
+    print ('Examples:')
+    print ('  android_icons_generator.py ic_launcher.png ~/awesome/app/src/main/res/mipmap')
+    print ('  android_icons_generator.py ic_person.png ~/awesome/app/src/main/res/drawable')
+    print ('')
     sys.exit(1)
 
 infile = sys.argv[1]    # e.g. ic_launcher.png
@@ -51,7 +51,7 @@ for (resolution, size) in dimensions:
         os.mkdir(out_dir)
 
     outfile = '%s/%s' % (out_dir, infile)
-    print 'Generating %s...' % outfile
+    print ('Generating %s...' % outfile)
     im.save(outfile, "PNG")
 
-print 'Done'
+print ('Done')
